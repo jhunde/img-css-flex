db741512cb4e02bbad53de5b705b345e09f2a4a1
hunde <jena.kelbessa@gmail.com>
2026-04-14 00:04:47 -0700

    Able to show img retrieved to the frontend

--- public/script.js
+++ public/script.js
@@ -15,16 +15,29 @@ async function fetchimage(){
         // Print data retrieved from api 
         grid.innerHTML = photos.map(photo => {
             // Load image to the front page
+            /*
             const media = 
                 photo.media_type === "image" 
-                    ? `<img src="${photo.image} alt="${photo.alt}" style="max-width:600px;">` 
-                    : `<a href="${photo.links}" target="_blank" rel="noreference">Open media</a>`;
+                    ? `<img src="${photo.image}" alt="${photo.alt}" style="max-width:600px;">` 
+                    : `<a href="${photo.link}" target="_blank" rel="noreference">Open media</a>`;
 
-                    return `
-                        <div class="items> 
-                            ${media}
-                        </div>
-                    `;
+                return `
+                    <div class="items> 
+                        <h2>${photo.alt} </h2>
+                        ${media}
+                    </div>
+                `;
+            */
+           const media =  photo.image
+            ? `<img src="${photo.image}" alt="${photo.alt || "unsplash photo"}" style="max-width:600px;">`
+            : `<a href="${photo.link}" target="_blank" rel="noreferrer">Open media</a>`;
+
+            return `
+                <div class="items">
+                    <h2> ${photo.alt || "unsplash photo"}</h2>
+                    ${media}
+                </div>
+            `;
 
         }).join("");
 
